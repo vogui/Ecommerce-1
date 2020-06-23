41ff4e2d2757f69baa57671355b7f2329878ffbe
MatiasRamosNOK <swhz05@protonmail.ch>
2020-06-23 11:55:53 -0300

    Principio de rutas

---
+++ Back/index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var session = require("express-session");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var routes = require("./routes");
+var app = express();
+var volleyball = require("volleyball");
+app.use(volleyball);
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api", routes);
+
+module.exports = app;
--- Back/routes/index.js
+++ Back/routes/index.js
@@ -1,13 +1,10 @@
 "use strict";
 const path = require("path");
-var express = require("express");
 var users = require("../routes/users");
+
 var router = express.Router();
 var passport = require("passport");
 // escriban sus rutas acá
-router.get("/users", function (req, res, next) {
-  // aca estaria en /api/users
-  express.use(users);
-});
+router.use("/users", users);
 
 module.exports = router;
