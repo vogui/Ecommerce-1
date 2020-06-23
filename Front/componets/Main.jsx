212a248aa225dd6f1c3ffdd1999b032eea89b893
vogui <guillevogemedialdea@gmail.com>
2020-06-23 18:26:12 -0300

    Commit de React

---
+++ Front/componets/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Switch, Router } from 'react-router-dom'
+
+
+export default ()=>{
+    return(
+      <Switch>
+          <Router />
+      </Switch>
+        
+    )
+
+}
---
+++ Front/store/reducer/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+import products from './input-reducer'
+
+
+export default combineReducers({
+    products
+})
