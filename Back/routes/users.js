3afbcb1970d1b44d9ec13f4b0de7ab9e972f0e77
MatiasRamosNOK <swhz05@protonmail.ch>
2020-06-23 12:43:49 -0300

    Routes

--- Back/routes/users.js
+++ Back/routes/users.js
@@ -1,9 +1,50 @@
 var express = require("express");
 var router = express.Router();
 var path = require("path");
-
+const { userInfo } = require("os");
+var User = require("user"); // Esto se modifica cuando Henry me diga como se llama el modelo de User
+var passport = require("passport");
 router.get("/", (req, res, next) => {
   //Aca llegan de /api/users/
+});
+
+router.post("/register", (req, res, next) => {
+  User.create(req.body)
+    .then((user) => {
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      res.status(502).send(err);
+    });
+});
+
+router.get("/login", function (req, res, next) {
+  console.log("ESTA AUTENTICADO:", req.isAuthenticated());
+  if (req.isAuthenticated()) {
+    console.log("Registro y login OK");
+    //Definir que hacer si el usuario esta logeado
+  } else {
+    //Definir que hacer si el usuario no esta logeado
+  }
+});
+
+router.post("/toAdmin/:id", (req, res, next) => {
+  User.findOne({
+    where: {
+      id: req.params.id,
+    },
+  }).then((user) => {
+    if (user.dataValues.isAdmin) {
+      User.update({ isAdmin: true }, { where: { id: req.body.id } })
+        .then((result) => res.status(201).send(result))
+        .catch((err) => res.status(401).send(err));
+    }
+  });
+});
+
+router.get("/logout", (req, res, next) => {
+  req.logout();
+  res.redirect("/");
 });
 
 module.exports = router;
