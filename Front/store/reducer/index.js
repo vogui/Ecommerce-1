0aaeb5fbecf5ecb940d07feae10bee0891294c2c
Hernan Hlace <hernan@hmbrokers.com>
2020-06-24 16:51:17 -0300

    brunch vieja

--- Front/store/reducer/index.js
+++ Front/store/reducer/index.js
@@ -2,10 +2,9 @@
 import { combineReducers } from "redux";
 import productsReducer from "./products-reducer";
 import loginReducer from "./login-reducer";
-import productsReducer from './products-reducer'
+
 export default combineReducers({
   products: productsReducer,
   login: loginReducer,
-  products: productsReducer,
 });
 
