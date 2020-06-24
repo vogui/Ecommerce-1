debaa723684e092706ab24d9ca23de44ee6aa2e7
Hernan Hlace <hernan@hmbrokers.com>
2020-06-24 11:32:00 -0300

    base de datos lista

--- Back/models/Cart-Products.js
+++ Back/models/Cart-Products.js
@@ -7,23 +7,23 @@ class Cart_Product extends S.Model {}
 
 Cart_Product.init(
   {
-    /* user_id: {
-      type: S.STRING,
+    user_id: {
+      type: S.INTEGER,
       references: {
         model: User,
         key: "id",
       },
     },
     product_id: {
-      type: S.STRING,
+      type: S.INTEGER,
       references: {
         model: Product,
         key: "id",
       },
-    }, */
+    },
     quantity: {
       type: S.INTEGER,
-      allowNull: false,
+      defaultValue: 1,
       validate: {
         min: 1,
       },
@@ -31,8 +31,5 @@ Cart_Product.init(
   },
   { sequelize: db, modelName: "Cart_Product" }
 );
-
-//Cart_Product.hasOne(User);
-//Cart_Product.hasMany(Product);
 
 module.exports = Cart_Product;
